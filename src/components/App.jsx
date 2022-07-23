646bbb1283b1fadf9be78ddbc6a7078aa033fc7b
tolyakryv <jp.komod.su@gmail.com>
2022-07-23 19:14:56 +0300

    changeFilter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
-
+import { useGetAllContactsQuery } from 'services/contactsAPI';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import ContactList from './ContactList';
 
 const App = () => {
-  return (
-    <div
-      style={{
-        display: 'block',
-        width: '450px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Phonebook</h1>
-      <ContactForm></ContactForm>
-      <h2>Contacts</h2>
-      <Filter></Filter>
-      <ContactList></ContactList>
-    </div>
-  );
-};
+  const { data } = useGetAllContactsQuery();
 
+  if (data) {
+    const contactLength = data.length;
+    console.log(contactLength);
+
+    return (
+      <div
+        style={{
+          display: 'block',
+          width: '450px',
+          margin: '0 auto',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm></ContactForm>
+        <h2>Contacts</h2>
+        {contactLength > 1 && <Filter></Filter>}
+        <ContactList></ContactList>
+      </div>
+    );
+  }
+};
 export default App;
